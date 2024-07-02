aab18c24f5d16ed5278dff8c5702aff69f27c9a8
kehindebello <adedotunomomeji@gmail.com>
2024-07-02 20:55:54 +0100

    add: app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+import json
+import requests
+import os
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    visitor_name = request.args.get("visitor")
+    client_ip = request.remote_addr
+    response = requests.get(f"https://ipgeolocation.abstractapi.com/v1/?api_key={os.getenv('ABSTRACT_API')}&ip_address={client_ip}")
+    city = response.json()['city']
+
+    #get weather data
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv('OPENWEATHER_KEY')}")
+    data = json.loads(response.text)
+    print(f'Data - {data}')
+    temperature = data['main']['temp'] - 273.15  # Convert Kelvin to Celsius
+
+    # Print the greeting message
+    print({"message": f"Hello {visitor_name}, the temperature is {temperature:.1f}°C in {city}"})
+    return jsonify({"message": f"Hello {visitor_name}, the temperature is {temperature:.1f}°C in {city}"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
